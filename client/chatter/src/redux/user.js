06ea942bd944cb631851bc9a2bb6257ba9847250
Harish Rana <r.shivi@yahoo.com>
2020-04-06 19:29:25 +0530

    feat: add feature type structure

---
+++ client/chatter/src/redux/user.js
@@ -0,0 +1,38 @@
+const INITIAL_STATE = {
+  isLoggedIn: false,
+  token: '',
+  loader: {
+    login: false,
+    registration: false,
+  },
+  errors: {
+    login: undefined,
+    registration: undefined,
+  },
+};
+
+// actions
+const LOGIN_START = 'APP/USER/LOGIN_START';
+const LOGIN_SUCCESS = 'APP/USER/LOGIN_SUCCESS';
+const LOGIN_ERROR = 'APP/USER/LOGIN_ERROR';
+
+const REGISTRATION_START = 'APP/USER/REGISTRATION_START';
+const REGISTRATION_SUCCESS = 'APP/USER/REGISTRATION_SUCCESS';
+const REGISTRATION_ERROR = 'APP/USER/REGISTRATION_ERROR';
+
+const LOGOUT_START = 'APP/USER/LOGOUT_START';
+export const LOGOUT_SUCCESS = 'APP/USER/LOGOUT_SUCCESS';
+const LOGOUT_ERROR = 'APP/USER/LOGOUT_ERROR';
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case [LOGIN_START]: {
+      return {
+        ...state,
+        action,
+      };
+    }
+    default:
+      return state;
+  }
+};
